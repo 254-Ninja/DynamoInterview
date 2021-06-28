10f0e27173d6c4b2d805027f2e5036c6138cacb4
254-ninja <muthomidenis62@gmail.com>
2021-06-28 14:50:46 +0300

    first commit

---
+++ src/house.java
@@ -0,0 +1,17 @@
+
+public class house {
+
+int size;
+String house;
+
+void Excutive () {
+	
+	if (size > 5000) {
+		System.out.println("Excutive house");
+		
+	}else if (size <5000) {
+		
+		System.out.println("Modern simple");
+	}	
+}
+}
